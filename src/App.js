6b8109e13668b05004217353509397821ddf4579
Stephane Mensah <waptik@users.noreply.github.com>
2021-10-13 20:09:54 +0000

    set cooldown limit error message

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import './App.css';
 import ClapPortalJson from './utils/ClapPortal.json'
@@ -9,35 +9,35 @@ export default function App() {
   const [isMining, setIsMining] = useState(false)
   const [allClaps, setAllClaps] = useState([])
   const [message, setMessage] = useState('')
-  const [error, setError]= useState(null)
+  const [error, setError] = useState(null)
 
 
-  const contractAddress= "0x70FFF133402577AaD0450E2823a76e973666adE3"
+  const contractAddress = "0xB04740b01A6EaDDeC11Ce1Bf5E4974738984901F"
   const contractABI = ClapPortalJson.abi
 
-  
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    const signer = provider.getSigner()
-    const clapPortalContract = new ethers.Contract(contractAddress, contractABI, signer)
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const signer = provider.getSigner()
+  const clapPortalContract = new ethers.Contract(contractAddress, contractABI, signer)
 
 
 
-    
+
 
 
   const checkIfEthereumIsAvailable = () => {
 
     //let's check if ethereum is available in the browser
-    const {ethereum} = window
+    const { ethereum } = window
 
-    if(!ethereum) {
+    if (!ethereum) {
       alert("You need to have metamask or similar extension installed in your browser");
       return
     }
 
     // check for account linked the ethereum
-    ethereum.request({method:"eth_accounts"}).then(accounts => {
-      if(accounts.length>0) {
+    ethereum.request({ method: "eth_accounts" }).then(accounts => {
+      if (accounts.length > 0) {
         const account = accounts[0]
 
         setCurrentAccount(account)
@@ -48,15 +48,15 @@ export default function App() {
   }
 
   const connectWallet = () => {
-    const {ethereum} = window
+    const { ethereum } = window
 
-    if(!ethereum) {
+    if (!ethereum) {
       alert("Install Metamask or similar extension")
       return
     }
 
-    ethereum.request({method:"eth_requestAccounts"}).then(accounts => {
-      if(accounts.length >0) {
+    ethereum.request({ method: "eth_requestAccounts" }).then(accounts => {
+      if (accounts.length > 0) {
         const account = accounts[0]
         console.log("Account connected: ", account)
         setCurrentAccount(account)
@@ -66,39 +66,53 @@ export default function App() {
   }
 
   const clap = async () => {
-    if(message === '') {
-      setError('Sorry, please add a message')
-      return;
+    try {
+      if (message === '') {
+        setError('Sorry, please add a message')
+        return;
+      }
+      setIsMining(true)
+      const clapTxn = await clapPortalContract.clap(message)
+      await clapTxn.wait()
+      const count = await clapPortalContract.getTotalClaps()
+      setCountClaps(count.toNumber())
+      setIsMining(false)
+      setMessage('')
+    } catch (err) {
+      if (err.message.includes("Cooldown")) {
+        const split = err.error.message.split("|")
+        console.log({ split, err })
+        setError(`Sorry, the action "clap" cannot be done at this moment. Please try again after ${split[1]}`)
+      } else {
+        setError(`Sorry, you cannot clap at this moment because: ${err.message}`)
+      }
+
+      setIsMining(false)
     }
-    setIsMining(true)
-    const clapTxn = await clapPortalContract.clap(message)
-    await clapTxn.wait()    
-    const count = await clapPortalContract.getTotalClaps()
-     setCountClaps(count.toNumber())
-    setIsMining(false)
-    setMessage('')
+
+
   }
-  
+
 
   useEffect(() => {
     checkIfEthereumIsAvailable()
 
-   const getTotalClaps = async () => {
-    const count = await clapPortalContract.getTotalClaps()
-     setCountClaps(count.toNumber())
-  }
-  getTotalClaps()
+    const getTotalClaps = async () => {
+      const count = await clapPortalContract.getTotalClaps()
+      setCountClaps(count.toNumber())
+    }
+    getTotalClaps()
   }, [clapPortalContract])
 
   useEffect(() => {
     const getAllClaps = async () => {
       try {
-        const {ethereum} = window
-        if(!ethereum) {
+        const { ethereum } = window
+        if (!ethereum) {
           console.log("Ethereum object doesn't exist!")
           return
         }
-        
+
         const claps = await clapPortalContract.getAllClaps()
 
         let clapsCleaned = []
@@ -112,7 +126,7 @@ export default function App() {
         })
 
         setAllClaps(clapsCleaned)
-      } catch(error) {
+      } catch (error) {
         console.log(error);
 
       }
@@ -120,43 +134,43 @@ export default function App() {
 
     }
 
-    if(currentAccount)
-    getAllClaps()
+    if (currentAccount)
+      getAllClaps()
   }, [clapPortalContract, currentAccount])
 
 
-  useEffect(()=> {
-    if(error) {
+  useEffect(() => {
+    if (error) {
       alert(error)
     }
   }, [error])
-  
+
   return (
     <div className="mainContainer">
 
       <div className="dataContainer">
         <div className="header">
-         <span role="img" aria-label="wave-emoji">👋</span> Heya fellas!
+          <span role="img" aria-label="wave-emoji">👋</span> Heya fellas!
         </div>
 
         <div className="bio">
-        Steve here, getting started into the world of crypto. Sounds fun right? Connect your Ethereum wallet and clap for me if you like what i do!
+          Steve here, getting started into the world of crypto. Sounds fun right? Connect your Ethereum wallet and clap for me if you like what i do!
         </div>
 
         {currentAccount ? (
-          
+
           <>
-          <span>Message</span> <textarea onChange={e => {
-            e.preventDefault();
-            const text = e.target.value
-            setMessage(text)
-          }} />
-          <button className="clapButton" disabled={isMining} onClick={clap}>
-          Clap for Me
-        </button> 
-        </>
-        ): <button className="clapButton" onClick={connectWallet}>
-          Connect your wallet
+            <span>Message</span> <textarea value={message} onChange={e => {
+              e.preventDefault();
+              const text = e.target.value
+              setMessage(text)
+            }} />
+            <button className="clapButton" disabled={isMining} onClick={clap}>
+              Clap for Me
+        </button>
+          </>
+        ) : <button className="clapButton" onClick={connectWallet}>
+            Connect your wallet
         </button>
         }
 
